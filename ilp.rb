d5cecbbedb27cb206e4aa752200220bdf560e908
me <li3939108@gmail.com>
2014-06-01 05:58:39 -0500

    multi platform

--- ilp.rb
+++ ilp.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 
-require './ILP.so'
+require './ILP.so' if RUBY_PLATFORM.downcase.include?("linux")
+require './ILP.dylib' if RUBY_PLATFORM.downcase.include?("darwin")
 
 def ilp(a, op, b, c, min, method)
 	self.send(method, a, op, b, c, min)
