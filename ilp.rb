51601d690a8a4dfffbf041f1108c574143059fb2
me <li3939108@gmail.com>
2014-06-01 06:11:39 -0500

    ruby 1.9+ require_relative

--- ilp.rb
+++ ilp.rb
@@ -1,7 +1,6 @@
 #! /usr/bin/env ruby
 
-require './ILP.so' if RUBY_PLATFORM.downcase.include?("linux")
-require './ILP.dylib' if RUBY_PLATFORM.downcase.include?("darwin")
+require_relative 'ILP'
 
 def ilp(a, op, b, c, min, method)
 	self.send(method, a, op, b, c, min)
