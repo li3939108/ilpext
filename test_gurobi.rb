1c0b2db2e4aa91c6816212ef2d22c6011a818e86
me <li3939108@gmail.com>
2014-06-01 21:40:09 -0500

    add file

---
+++ test_gurobi.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/env  ruby
+# 2014 by Chaofan Li <chaof@tamu.edu>
+
+require_relative 'ilp'
+
+#solve the ILP problem
+
+# min(max)  c   x
+#           a   x   op   b
+#           int x
+
+c=[	0,	1]
+a=[	[-1,	1], 
+	[3,	2], 
+	[2,	3]]
+op=[	LE, 
+	EQ,
+	LE]
+#rhs
+b=[	1, 
+	10, 
+	12]
+print ilp(a, op, b, c, :max, :gurobi), "\n"
